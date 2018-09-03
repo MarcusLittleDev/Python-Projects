02b1054a8b21b2375518997505ca21091943a269
marcus little <marcus.little.selu.edu>
2018-09-02 23:17:07 -0500

    added permissions and authentication

--- Django-Api/profiles-rest-api/src/profiles_project/profiles_api/views.py
+++ Django-Api/profiles-rest-api/src/profiles_project/profiles_api/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from . import serializers
-from . import models
+from . import serializers, models, permissions
 
 # Create your views here.
 
@@ -104,3 +104,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
