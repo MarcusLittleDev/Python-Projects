8d74886fa2f82e13967ce12bfbecb39a19ef227b
marcus little <marcus.little.selu.edu>
2018-09-02 22:06:39 -0500

    added viewset and router

--- Django-Api/profiles-rest-api/src/profiles_project/profiles_api/urls.py
+++ Django-Api/profiles-rest-api/src/profiles_project/profiles_api/urls.py
@@ -1,8 +1,13 @@
 from . import views
-from django.urls import path
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('Hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
